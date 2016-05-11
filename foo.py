6d21a1dfba75332336cdccd27b2e8a40f512b7b8
DoneWithFish <bfitzpat.math.duke@gmail.com>
2016-05-11 02:16:19 -0400

    added python code

--- foo.py
+++ foo.py
@@ -1,7 +1,6 @@
 from pick import pick
 from datetime import date
-import sys
-import os
+import ConfigParser, os, subprocess, sys
 
 def choose_year():
     if date.today().month > 10:
@@ -14,12 +13,6 @@ def choose_year():
     return chosen_year
 
 def choose_semester():
-    dic = {
-        'Fall'          : 'fall',
-        'Spring'        : 'spring',
-        'Summer Term 1' : 'summer-t1',
-        'Summer Term 2' : 'summer-t2'
-    }
     semesters   = ['Fall', 'Spring',  'Summer Term 1',  'Summer Term 2']
     semester, i = pick(semesters, 'Choose semester.', indicator = '=>', default_index = 0)
     return semester
@@ -42,30 +35,72 @@ def abbreviate_term(year, semester):
     term_abbrev = year.replace('20', '', 1) + '-' + dic[semester]
     return term_abbrev
 
-def make_course():
-    semester       = choose_semester()
-    year           = choose_year()
-    course_number  = choose_course_number()
-    section_number = choose_section_number()
-
-    print "You chose year: {}".format(year)
-    print "You chose semester: {}".format(semester)
-    print "You chose section number: {}".format(section_number)
-    print "Your term abbreviation: {}".format(abbreviate_term(year, semester))
-    print "Your course: Math {}".format(course_number)
-
 def find_roster():
     if len(sys.argv) > 1:
         path = str(sys.argv[1])
     else:
         default_path = '/home/brian/downloads/ps.xls'
         path = raw_input("Input path to roster [{}]: ".format(default_path)) or default_path
-    print os.path.isfile(path)
 
-find_roster()
+    while not os.path.isfile(path):
+        default_path = '/home/brian/downloads/ps.xls'
+        path = raw_input("Path invalid. Try again [{}]: ".format(default_path)) or default_path
 
+    return path
 
-# title = 'Welcome to myClass!\n\nWhat would you like to do?'
-# options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
-# option, index = pick(options, title, indicator='=>', default_index=2)
-# print(option, index)
+def make_roster_file(roster_path, new_path):
+    command = "ssconvert {0} {1}".format(roster_path, new_path)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+
+def make_dirs(l):
+    for x in l:
+        if not os.path.exists(x):
+            os.makedirs(x)
+
+# def make_labels():
+#     csv.DictReader(open())
+#     target = open(file_path, 'w')
+#     target.write("\documentclass[]{letter}\n")
+#     target.write("\usepackage[avery5160label,noprintbarcodes,nocapaddress]{envlab}\n")
+#     target.write("\makelabels\n")
+#     target.write("\begin{document}\n")
+#     target.write("\startlabels\n")
+
+#     target.write("\mlabel{}{\texttt{Ryan Alexander Bloom\\Section 4}}\n")
+#     target.write("\end{document}\n")
+
+#     target.close()
+
+def make_course():
+    semester       = choose_semester()
+    year           = choose_year()
+    course_number  = choose_course_number()
+    section_number = choose_section_number()
+    roster_path    = find_roster()
+
+    term_abbrev = abbreviate_term(year, semester)
+
+    config = ConfigParser.RawConfigParser()
+    config.read('.dirs.cfg')
+
+    teaching_path  = config.get('dirs', 'teaching_directory')
+    term_path      = teaching_path + term_abbrev
+    course_path    = term_path + '/math-' + course_number
+    exams_path     = course_path + '/exams'
+    info_path      = course_path + '/info'
+    gradebook_path = course_path + '/gradebook'
+    scratch_path   = course_path + '/scratch'
+    
+    make_dirs([
+        teaching_path, 
+        term_path, 
+        course_path, 
+        exams_path, 
+        info_path, 
+        gradebook_path, 
+        scratch_path
+    ])
+    make_roster_file(roster_path, info_path + '/sec{}-roster.csv'.format(section_number))
+
+make_course()
